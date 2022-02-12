8c24962e98939f42438b1253110a95245750f86f
jg93pt <jorgebenf@gmail.com>
2022-02-12 02:00:47 +0000

    Responsive Carousel created for Work Section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //Carousel in Work Section
+//Carousel for HTML and CSS Development
+var carouselHTMLANDCSS = function () {
 var multipleCardCarousel = document.querySelector(
-  "#carouselControls"
+  "#carouselHTMLCSS"
 );
 if (window.matchMedia("(min-width: 768px)").matches) {
   var carousel = new bootstrap.Carousel(multipleCardCarousel, {
@@ -9,19 +11,19 @@ if (window.matchMedia("(min-width: 768px)").matches) {
   var carouselWidth = $(".carousel-inner")[0].scrollWidth;
   var cardWidth = $(".carousel-item").width();
   var scrollPosition = 0;
-  $("#carouselControls .carousel-control-next").on("click", function () {
+  $("#carouselHTMLCSS .carousel-control-next").on("click", function () {
     if (scrollPosition < carouselWidth - cardWidth * 4) {
       scrollPosition += cardWidth;
-      $("#carouselControls .carousel-inner").animate(
+      $("#carouselHTMLCSS .carousel-inner").animate(
         { scrollLeft: scrollPosition },
         600
       );
     }
   });
-  $("#carouselControls .carousel-control-prev").on("click", function () {
+  $("#carouselHTMLCSS .carousel-control-prev").on("click", function () {
     if (scrollPosition > 0) {
       scrollPosition -= cardWidth;
-      $("#carouselControls .carousel-inner").animate(
+      $("#carouselHTMLCSS .carousel-inner").animate(
         { scrollLeft: scrollPosition },
         600
       );
@@ -30,3 +32,79 @@ if (window.matchMedia("(min-width: 768px)").matches) {
 } else {
   $(multipleCardCarousel).addClass("slide");
 }
+};
+
+//Carousel for JS Programming
+var carouselJS = function () {
+  var multipleCardCarousel = document.querySelector(
+    "#carouselJS"
+  );
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+    });
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouselJS .carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 4) {
+        scrollPosition += cardWidth;
+        $("#carouselJS .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $("#carouselJS .carousel-control-prev").on("click", function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $("#carouselJS .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
+  };
+
+  //Carousel for Dynamic Web Development
+var carouselDynamic = function () {
+  var multipleCardCarousel = document.querySelector(
+    "#carouseldynamic"
+  );
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false,
+    });
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouseldynamic .carousel-control-next").on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth * 4) {
+        scrollPosition += cardWidth;
+        $("#carouseldynamic .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+    $("#carouseldynamic .carousel-control-prev").on("click", function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $("#carouseldynamic .carousel-inner").animate(
+          { scrollLeft: scrollPosition },
+          600
+        );
+      }
+    });
+  } else {
+    $(multipleCardCarousel).addClass("slide");
+  }
+  };
+
+//Initialize functions:
+carouselHTMLANDCSS();
+carouselJS();
+carouselDynamic();
